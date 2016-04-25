2c4d3c8024dda0db39b5bc1da14ffb0c3a435e85
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-04-25 17:56:17 -0500

    first dynamic webpage done

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,9 @@
+class FortunesController < ApplicationController
+
+  def lucky
+    #method name has to be the same as the action defined in routes.rb
+    @num = rand(100)
+    render("zebra.html.erb")
+    #allows rails to use the contents of the html/css file to format the output on the user's end
+  end
+end
