c735dbc7fae47a64ec8d84d7886c961ebd0b3b38
jillseidel <jseidel2017@users.noreply.github.com>
2016-04-28 20:44:52 -0500

    Fixed the lucky and unlucky numbers pages

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,16 +1,20 @@
 class FortunesController < ApplicationController
 
   def lucky
-    @num = rand(100)
+    @first_number = rand(100)
+    @second_number = rand(100)
+    @third_number = rand(100)
+    @fourth_number = rand(100)
+    @fifth_number = rand(100)
 
-    render("zebra.html.erb")
+    render("lucky.html.erb")
 
   end
-  
-  def unlucky
-    @num = rand(100)
 
-    render("giraffe.html.erb")
+  def unlucky
+    @numbers = [rand(100...1000),rand(100...1000),rand(100...1000),rand(100...1000),rand(100...1000)]
+
+    render("unlucky.html.erb")
 
   end
 
