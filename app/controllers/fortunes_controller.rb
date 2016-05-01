24479ed1df543fead5696b04f046c7c3780a6d61
elaineichen <elainechen2017@kellogg.northwestern.edu>
2016-05-01 18:45:00 -0500

    fortune teller complete

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,22 @@
+class FortunesController < ApplicationController
+  def lucky
+    @first_number = rand(100)
+    @second_number = rand(100)
+    @third_number = rand(100)
+    @fourth_number = rand(100)
+    @fifth_number = rand(100)
+
+    render ("lucky.html.erb")
+  end
+
+  def unlucky
+    @first_number = rand(100)
+    @second_number = rand(100)
+    @third_number = rand(100)
+    @fourth_number = rand(100)
+    @fifth_number = rand(100)
+
+    render ("unlucky.html.erb")
+  end
+
+end
