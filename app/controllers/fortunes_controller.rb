dbcc977db411bdd31b1170a20abd6e4b1f961ed7
achen2017 <achen2017>
2016-05-01 21:37:29 -0500

    bugs fixed I think

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,15 @@
+class FortunesController < ApplicationController
+
+  def lucky
+      render("zebra.html.erb")
+  end
+  def unlucky
+      @unlucky_numbers = [rand(500...1000), rand(500...1000), rand(500...1000), rand(500...1000)]
+      render("cobra.html.erb")
+
+  end
+
+
+
+
+end
