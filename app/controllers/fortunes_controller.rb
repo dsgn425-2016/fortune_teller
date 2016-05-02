577981f09a268053a42de9e2198c99014d1d75f5
yeturum <yeturum@gmail.com>
2016-05-01 19:17:27 -0500

    ManasaYeturu_fortuneteller_final

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,9 +1,17 @@
 class FortunesController < ApplicationController
 
 def lucky
-  num = rand(100)
+  @num = rand(100)
 
   render("lucky.html.erb")
 end
 
+end
+
+def unlucky
+
+  @num = rand(600)
+
+  render("unlucky.html.erb")
+end
 end
