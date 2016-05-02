e1d3125d5a495974ea75030c630201ca2b701f3b
OLiles2 <orlandoliles@Orlandos-MacBook-Air.local>
2016-05-02 02:28:19 -0500

    actaully_ready_for_review

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -68,6 +68,6 @@ class ZodiacsController < ApplicationController
   def fish
     @prediction = "Business and financial success makes you happy and satisfied, Pisces. You also look forward to moving on. The downside of this flush period is that people who aren't particularly trustworthy might decide to latch onto your coattails for their own purposes. Some might even ask for loans. Be careful about the ones you assist. They might not be honest with you. Don't fall for any sob stories."
 
-    render("scales.html.erb")
+    render("fish.html.erb")
   end
 end
